929b6241cae8475dbfd7c912ac77e58cc87a46c0
Fiza Mirza <fizamirza1113@gmail.com>
2026-02-25 14:44:43 +0530

    add code

---
+++ Backend/App/Core/Security.py
@@ -0,0 +1,9 @@
+# Define two numbers
+num1 = 10
+num2 = 20
+
+# Add the numbers using the '+' operator
+sum_result = num1 + num2
+
+# Display the sum
+print(f"The sum of {num1} and {num2} is: {sum_result}")
