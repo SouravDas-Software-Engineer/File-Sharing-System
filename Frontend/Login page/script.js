408c0121ba9933ecbd6dedad80088641f90366e5
Asif-source-lgtm <asifhasansaif76@gmail.com>
2026-03-03 00:33:45 +0530

    update script.js

--- Frontend/Login page/script.js
+++ Frontend/Login page/script.js
@@ -1,18 +1,28 @@
-// Select elements
 let loginButton = document.getElementById("loginBtn");
 let emailInput = document.getElementById("email");
 let passwordInput = document.getElementById("password");
+let message = document.getElementById("message");
 
-// Add click event
 loginButton.addEventListener("click", function() {
 
     let email = emailInput.value;
     let password = passwordInput.value;
 
     if(email === "" || password === "") {
-        alert("Please fill all fields");
-    } else {
-        alert("Login Successful");
+        message.textContent = "Please fill all fields";
+        message.style.color = "red";
+    }
+    else if(!email.includes("@")) {
+        message.textContent = "Enter valid email";
+        message.style.color = "red";
+    }
+    else {
+        message.textContent = "Login Successful";
+        message.style.color = "green";
+
+        // Clear fields
+        emailInput.value = "";
+        passwordInput.value = "";
     }
 
 });
