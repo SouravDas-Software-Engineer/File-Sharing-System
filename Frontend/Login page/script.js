7677718a451fe0b0a55e5200c279702fbfd5268b
Fiza Mirza <fizamirza1113@gmail.com>
2026-03-04 19:47:56 +0530

    Api connection

--- Frontend/Login page/script.js
+++ Frontend/Login page/script.js
@@ -49,3 +49,11 @@ function toggleMode() {
 document.addEventListener("DOMContentLoaded", () => {
     document.body.classList.add("dark");
 });
+
+const response = await fetch("http://127.0.0.1:8000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email: email, password: password })
+});
+
+const data = await response.json();
